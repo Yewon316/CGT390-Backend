eebd117ec6ba01d68cd60c9f3b26bd9a7140ca2c
Yewon316 <readchoi@naver.com>
2025-10-31 10:21:03 -0400

    Basic Setting + Lab 14

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: "About",
+};
+
+    export default function AboutPage() {
+        return (
+        <section style={{ maxWidth: 720, margin: "0 auto" }}>
+            <h1>About</h1>
+            <p>
+                This is about page.
+            </p>
+        </section>
+    );
+}
